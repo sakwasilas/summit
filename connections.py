18a3ad2779c2984a90f9b259523b4906c7a5c85c
sakwasilas <sakwahsilas@gmail.com>
2025-09-05 17:25:38 +0300

    created connection.py

--- connections.py
+++ connections.py
@@ -1,32 +1,17 @@
 
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import sessionmaker, scoped_session, declarative_base
-
-# # ✅ Correct MySQL path format
-# DATABASE_URL = "mysql+mysqldb://root:2480@localhost:3306/summit_db"
-
-# engine = create_engine(DATABASE_URL, pool_pre_ping=True)
-
-# Session = scoped_session(sessionmaker(bind=engine))
-# SessionLocal = Session
-
-# Base = declarative_base()
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session, declarative_base
 
-# Database URL
-DATABASE_URL = "postgresql://summit_lxsj_user:AwgCE7QG9BGI8UYzT6rNsThjUNDp0fAt@dpg-d2tc8cre5dus73dmuqpg-a.oregon-postgres.render.com:5432/summit_lxsj?sslmode=require"
 
-# Engine
+
+DATABASE_URL=("postgresql://summit:1YT0CF5RUqDdmNo0bXaACWwI66j9FMLE@dpg-d2te6495pdvs739fu590-a.oregon-postgres.render.com:5432/summit_6gqd?sslmode=require"
+)
+
 engine = create_engine(DATABASE_URL)
 
-# Session
+
 Session = scoped_session(sessionmaker(bind=engine))
 SessionLocal = Session
 
-# Base
+
 Base = declarative_base()
-
-
-
