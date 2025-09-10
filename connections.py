11315598c482259d4c17e75c6dbf716ef8b5b01a
sakwasilas <sakwahsilas@gmail.com>
2025-09-10 08:16:49 +0300

    modified database url

--- connections.py
+++ connections.py
@@ -16,17 +16,29 @@
 
 # Base = declarative_base()
 
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker, scoped_session, declarative_base
+
+# # MySQL connection string without host/port (defaults to localhost:3306)
+# DATABASE_URL = "mysql+mysqldb://root:2480@/summit_db"
+
+# engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+
+# Session = scoped_session(sessionmaker(bind=engine))
+# SessionLocal = Session
+
+# Base = declarative_base()
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session, declarative_base
 
-# MySQL connection string without host/port (defaults to localhost:3306)
-DATABASE_URL = "mysql+mysqldb://root:2480@/summit_db"
+# Updated database URL
+DATABASE_URL = "postgresql://summit_r1g6_user:VQFcCbzmgEnziJTbnX51a0SBgVn81FUB@dpg-d30gcp7fte5s73ecjk3g-a.oregon-postgres.render.com:5432/summit_r1g6?sslmode=require"
 
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+engine = create_engine(DATABASE_URL)
 
 Session = scoped_session(sessionmaker(bind=engine))
 SessionLocal = Session
 
 Base = declarative_base()
-
 
