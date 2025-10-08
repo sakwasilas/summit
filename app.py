2f3fc1e69a42bcbb2b7a90170379a9958fca70d8
sakwasilas <sakwahsilas@gmail.com>
2025-10-08 16:42:46 +0300

    added production port

--- app.py
+++ app.py
@@ -1240,8 +1240,5 @@ def student_results():
         db.close()
 
 
-# -----------------------------
-# Run
-# -----------------------------
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
